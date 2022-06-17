b39ec6d8b2583108c14d38d824d5de71243a5c8e
Dương Unity (Jake) <jakediep1902@gmail.com>
2022-06-17 15:28:41 +0700

    modify
    add button "Login Guest"
    new player have initial money changed to 5M.

--- Assets/Scripts/UserData.cs
+++ Assets/Scripts/UserData.cs
@@ -9,10 +9,11 @@ public class UserData
     public string userName;
     public string[] namesTemplate = { "Distintion","Canary","Solomo","Sizuka","Hwking","Constraint","Sophisticate","Darwin","Computer",
     "Passion","Absurb","Investigate","Transformer","ComanDos","coverage","ImBot","Mohamad","Tomato","Apple","pinoco","SisaGia","BaCoGai","DaiBay",
-    "ThichAnChoi","unity","UE44Kay","Flying","28Sich","TheKing","QueenWin"};
+    "ThichAnChoi","unity","UE44Kay","Flying","28Sich","TheKing","QueenWin","Xibachao","asjd134j","sjai453ia","Marval","spikeder","richjack","cutoi",
+    "allIn","noName","choi0vui","ramSay","ulukahn","sinHat","hongKong","chines","sisily","poscasi","dragon","angle"};
     public long money;
 
-    public UserData(string name = "",long money = 200000)
+    public UserData(string name = "",long money = 5000000)
     {
         this.money = money;
         userName = name;       
